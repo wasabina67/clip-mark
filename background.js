90ca5d1d473fb3ff3610513b089d5ccfb22b6c45
wasabina67 <wasabina67@gmail.com>
2025-05-31 08:59:00 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,17 @@
 
-chrome.runtime.onInstalled.addListener(() => {});
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "hello-world",
+    title: "Hello World Extension",
+    contexts: ["all"],
+  });
+});
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {});
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "hello-world" && tab.id) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => alert("Hello World!"),
+    });
+  }
+});
